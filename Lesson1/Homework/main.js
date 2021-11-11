e61cd055909287ee346176fd02439ed9eeadf150
IhorStets <stets237@gmail.com>
2021-11-11 17:26:48 +0200

    Initial commit

---
+++ Lesson1/Homework/main.js
@@ -0,0 +1,4 @@
+// - Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+//     Вивести кожну змінну за допомогою: console.log , alert, document.write
+let hello = "hello";
+console.log (hello);
