5fde0069c977038c57903e3918661cdc770fd6ac
IhorStets <stets237@gmail.com>
2021-12-13 00:49:05 +0200

    hW9

--- Lesson9/hW9.js
+++ Lesson9/hW9.js
@@ -31,25 +31,37 @@ arr.forEach ( item => {
 })
 //
 // - Є масив
-// let coursesAndDurationArray = [
-//     {title: 'JavaScript Complex', monthDuration: 5},
-//     {title: 'Java Complex', monthDuration: 6},
-//     {title: 'Python Complex', monthDuration: 6},
-//     {title: 'QA Complex', monthDuration: 4},
-//     {title: 'FullStack', monthDuration: 7},
-//     {title: 'Frontend', monthDuration: 4}
-// ];
+const coursesAndDurationArray = [
+    {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4}
+];
 // Для кожного елементу масиву зробити блок в якому вивести інформацію про title та monthDuration
 // Завдання робити через цикли.
 //
-// - Є масив
-// let coursesAndDurationArray = [
-//     {title: 'JavaScript Complex', monthDuration: 5},
-//     {title: 'Java Complex', monthDuration: 6},
-//     {title: 'Python Complex', monthDuration: 6},
-//     {title: 'QA Complex', monthDuration: 4},
-//     {title: 'FullStack', monthDuration: 7},
-//     {title: 'Frontend', monthDuration: 4}
-// ];
-// За допомоги скріпта для кожного елементу масиву зробити <div class='item'> ,  в якому буде <h1 class='heading'>  з title  елементу, та <p class='description'> з monthDuration елементу.
+coursesAndDurationArray.forEach(value =>{
+const div = document.createElement('div');
+div.innerText = `Курс ${value.title} триває місяців: ${value.monthDuration}`
+document.body.appendChild(div);
+})
+//
+// За допомоги скріпта для кожного елементу масиву зробити <div class='item'> ,  в якому буде <h1 class='heading'>
+// з title  елементу, та <p class='description'> з monthDuration елементу.
 //     Завдання робити через цикли.
+coursesAndDurationArray.forEach( value => {
+    const div = document.createElement('div');
+    div.classList.add('item');
+    const h1 = document.createElement('h1');
+    h1.classList.add('heading');
+    h1.style.fontSize = '26px'
+    const p = document.createElement('p');
+    p.classList.add('description');
+    p.style.fontSize = '20px'
+    h1.innerText = value.title;
+    p.innerText = `Триває місяців -- ${value.monthDuration}`;
+    div.append(h1,p);
+    document.body.appendChild(div)
+})
